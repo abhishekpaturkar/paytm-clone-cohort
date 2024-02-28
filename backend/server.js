51bcfbbc92130ae27b4e2275f5719c935aabcedf
abhishekpaturkar <godkillop03@gmail.com>
2024-02-28 18:40:25 +0530

    Cors added

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cors from "cors";
 import { connect } from "./database/connectDB.js";
 import router from "./routes/router.js";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(express.json());
 app.use("/api/v1", router);
 
